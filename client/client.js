df5965b4c01fce72d48b5fffa80fa6c2be0d0a8c
bbhupen <bbhupen601@gmail.com>
2022-07-16 00:47:25 +0530

    update client.js

--- client/client.js
+++ client/client.js
@@ -6,7 +6,7 @@ const button = document.getElementById('notifications');
 button.addEventListener('click', () => {
   Notification.requestPermission().then((result) => {
     if (result === 'granted') {
-      randomNotification();
+      console.log("Granted")
     }
   });
 })
