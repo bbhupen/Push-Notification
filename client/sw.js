2d9b43596de440c10c2f16d9b89151c450c843c5
bbhupen <bbhupen601@gmail.com>
2022-07-16 00:04:36 +0530

    update index.js

--- client/sw.js
+++ client/sw.js
@@ -1,4 +1,15 @@
+importScripts(
+  'https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js'
+);
+
+workbox.routing.registerRoute(
+  ({request}) => request.destination === 'image',
+  new workbox.strategies.CacheFirst()
+)
+
+
 console.log("Service worker loaded.....")
+
 
 self.addEventListener('push', event => {
   const data = event.data.json()
@@ -7,3 +18,4 @@ self.addEventListener('push', event => {
     body: data.body
   })
 })
+
