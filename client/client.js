aee8d163c0456cea52d48d74bd20f9a4979b324d
bbhupen <67063163+bbhupen@users.noreply.github.com>
2022-07-15 23:03:26 +0530

    commited

---
+++ client/client.js
@@ -0,0 +1,45 @@
+const publicVapidKey = "BLDWl3yIGDWt1r0ZBxTTbmBW4aWS_ANClYl3EOmxh7FAANHQWEJOS0lMSeGaoha65eXG2GppZbkI7ne5dVzpz3Y"
+
+//Register Service Worker
+if ('serviceWorker' in navigator) {
+    send().catch(err => console.error(err))
+}
+
+async function send() {
+    console.log("Register SW")
+
+    const register = await navigator.serviceWorker.register("/sw.js", { scope: '/' })
+    console.log("SW registered") 
+
+    const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(publicVapidKey)
+    })
+
+    await fetch("/subscribe", {
+        method: 'POST',
+        body: JSON.stringify(subscription),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+
+
+}
+
+
+
+function urlBase64ToUint8Array(base64String) {
+    const padding = "=".repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, "+")
+        .replace(/_/g, "/");
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const path = require('path')
+const bodyparser = require('body-parser')
+const webpush = require('web-push')
+
+const app = express()
+const port = 3000
+
+const publicVapidKey = "BLDWl3yIGDWt1r0ZBxTTbmBW4aWS_ANClYl3EOmxh7FAANHQWEJOS0lMSeGaoha65eXG2GppZbkI7ne5dVzpz3Y"
+
+const privateVapidKey= "rr5zeP0T29H_MxNRXPuCeYH4as4p8CxZD0liNOYQVyM"
+
+webpush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey)
+
+app.use(express.static(path.join(__dirname, "client")))
+app.use(bodyparser.json())
+
+
+app.get('/', (req, res) => {
+    res.sendFile("index.html")
+})
+
+
+app.post('/subscribe', (req, res) => {
+    const subscription = req.body
+    console.log(subscription)
+    res.status(201).json({})
+    const payload = JSON.stringify({title: "Yo this is a push notification hello world"})
+    webpush.sendNotification(subscription, payload).catch(err => console.error(err))
+})
+
+app.listen(port, () => 
+console.log(`Server started on port ${port}`))
+
+
+
